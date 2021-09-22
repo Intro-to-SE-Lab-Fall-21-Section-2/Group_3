a51028e410107abfbf47a98b49eeccb351a2a9bb
vinnykuhnel <vak58@msstate.edu>
2021-09-22 18:29:08 -0500

    Handles multiple mail servers

--- emailClient/login/views.py
+++ emailClient/login/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+class IMAP_server:
+    def __init__(self, dns: str, port: int):
+        self.dns = dns
+        self.port = port
+
+servers = dict([("Gmail", IMAP_server("smtp.gmail.com", 587)),
+                ("Outlook", IMAP_server("smtp-mail.outlook.com", 587)),
+                ("Yahoo", IMAP_server("smtp.mail.yahoo.com", 587)),
+                ("Comcast", IMAP_server("smtp.comcast.net", 587)),
+                ("Verizon", IMAP_server("smtp.verizon.net", 465))])
+
+
 # Create your views here.
 def login(request):
     return render(request, 'login/login.html')
@@ -8,5 +20,7 @@ def login(request):
 def authentication(request):
     email = request.GET.get('emailaddress')
     psswd = request.GET.get('password')
+    msp = request.GET.get('msp')
+    chosenIMAP = servers[msp]
     
     return render(request, 'login/authentication.html')
