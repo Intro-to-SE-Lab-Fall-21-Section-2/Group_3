f82b99542cb1cd44b6e0baebd090f64bd37f38b6
vinnykuhnel <vak58@msstate.edu>
2021-09-28 21:36:49 -0500

    added message returning capability

--- emailClient/login/views.py
+++ emailClient/login/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import imapclient
+import pyzmail
 
 class IMAP_server:
     def __init__(self, imap: str, smtp: str, port: int):
@@ -12,17 +15,33 @@ servers = dict([("Gmail", IMAP_server("imap.gmail.com", "smtp.gmail.com", 587)),
                 ("Comcast", IMAP_server("imap.comcast.net", "smtp.comcast.net", 587)),
                 ("Verizon", IMAP_server("incoming.verizon.net", "smtp.verizon.net", 465))])
 
+def IMAPlogin(username: str, password: str, server: str) -> imapclient:
+    imapObj = imapclient.IMAPClient(server, ssl=True)
+    try:
+        imapObj.login(username, password)
+        
+    except:
+        return None
+    return imapObj
+
+def mailRender(server: imapclient, mailID: int) -> str:
+    server.select_folder('INBOX', readonly=True)
+    rawMessage = server.fetch([mailID], ['BODY[]', 'FLAGS'])
+    email = pyzmail.PyzMessage.factory(rawMessage[mailID][b'BODY[]'])
+    return email.html_part.get_payload().decode(email.html_part.charset)
 
 # Create your views here.
 def login(request):
     return render(request, 'login/login.html')
-
 
 def authentication(request):
     email = request.GET.get('emailaddress')
     psswd = request.GET.get('password')
     msp = request.GET.get('msp')
     chosenMSP = servers[msp]
-    print(chosenMSP.imap)
-    
-    return render(request, 'login/authentication.html')
+    server = IMAPlogin(email, psswd, chosenMSP.imap)
+    if not server:    
+        return render(request, 'login/authentication.html')
+    email = mailRender(server, 9)
+    server.logout()
+    return HttpResponse(email)
